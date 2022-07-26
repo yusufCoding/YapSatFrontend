c77042def5c5ac6ba3b50de2d2fcf5b3df296caf
Serhad Kuş <serons3@outlook.com>
2022-07-27 00:08:56 +0300

    Giriş sayfası oluşturuldu

--- src/components/Serhadcomponent.js
+++ src/components/Serhadcomponent.js
@@ -1,78 +1,83 @@
 import React from 'react'
-import Carousel from 'react-bootstrap/Carousel';
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import orman from "./orman.jpg"
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import kralsero from "./Kralsero.jpg"
 
 const Serhadcomponent = () => {
   return (
     <div>
-    <Navbar bg="dark" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+    <Navbar style={{padding:'20px'}} bg="dark" variant="dark" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#">Yapsat</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll>         
+            <Nav.Link href="#action1">Home</Nav.Link>
+          </Nav>
+          <Nav>
+          <Nav.Link href="#action2">Üye ol</Nav.Link>
+          <Nav.Link href="#action2">Giriş</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-   
-<Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={orman}
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3 className="text-gray-900">First slide label</h3>
-          <p className="text-gray-900">Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={orman}
-          alt="Second slide"
-        />
 
-        <Carousel.Caption>
-          <h3 className="text-gray-900">Second slide label</h3>
-          <p className="text-gray-900">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={orman}
-          alt="Third slide"
-        />
+    <div>
+    <CardGroup className='text-center' style={{margin:'20px'}}>
+      <Card style={{padding:'20px'}}>
+        <Card.Img variant="top" src={kralsero}/>
+        <Card.Body>
+          <Card.Title>Kral Sero</Card.Title>
+          <Card.Text>
+            850rp
+          </Card.Text>
+        </Card.Body>
+      </Card>
 
-        <Carousel.Caption>
-          <h3 className="text-gray-900">Third slide label</h3>
-          <p className="text-gray-900">
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+      <Card style={{padding:'20px'}}>
+        <Card.Img variant="top" src={kralsero}/>
+        <Card.Body>
+          <Card.Title>Normal Sero</Card.Title>
+          <Card.Text>
+           850rp
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      </CardGroup>
+
+      <CardGroup className='text-center' style={{margin:'20px'}}>
+      <Card style={{padding:'20px'}}>
+        <Card.Img variant="top" src={kralsero}/>
+        <Card.Body>
+          <Card.Title>Kral Sero</Card.Title>
+          <Card.Text>
+            850rp
+          </Card.Text>
+        </Card.Body>
+      </Card>
+
+      <Card style={{padding:'20px'}}>
+        <Card.Img variant="top" src={kralsero}/>
+        <Card.Body>
+          <Card.Title>Normal Sero</Card.Title>
+          <Card.Text>
+           850rp
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      
+    </CardGroup>
     </div>
+    </div>
+
   );
 }
 
